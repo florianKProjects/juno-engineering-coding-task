321d2acbc0e0bd424ce98344ceeb2c87a653c435
florianKProjects <58300216+florianKProjects@users.noreply.github.com>
2022-05-09 22:34:03 +0300

    Taks2

--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -4,39 +4,71 @@ import { allIds, fetchOrderById } from "../api";
 ////////////////////////////////// Your code tasks is below //////////////////////////////////////////////////////
 
 const fetchAllOrders = () => {
-    const ids = allIds;
-    // .....
-    //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+  //get all the ids
+  const ids = allIds;
+  //loop over all the ids and get the orders
+  const resOrderPromises = ids.map((id) => fetchOrderById(id));
+  return Promise.all(resOrderPromises);
 };
 
-const bucketOrdersByUsers = () => {
-    let ordersByUsers = {};
-    //   2. TODO: using the function from section 1 you should now bucket the orders by user.
-    // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
-    return ordersByUsers;
+const bucketOrdersByUsers = async () => {
+  let ordersByUsers = {};
+  let bucketOrders = [];
+  //Get  all the orders of the users
+  await fetchAllOrders().then((data) => (bucketOrders = data));
+
+  //create list of hashKey,userid is the key and value is the order
+  bucketOrders.map((item) =>
+    !ordersByUsers[item.userId]
+      ? (ordersByUsers[item.userId] = [item])
+      : ordersByUsers[item.userId].push(item)
+  );
+
+  return ordersByUsers;
+};
+const getLast2WeeksOrders = async () => {
+  let orders = [];
+  const numberOfWeeks = 2;
+  //Get  all the orders of the users
+  const timestamp2WeeksAgo =
+    new Date() - 1000 * 60 * 60 * 24 * (numberOfWeeks * 7); // last 2 weeks
+
+  await fetchAllOrders().then((data) => (orders = data));
+  const ordersByLast2Weeks = orders.filter((order) => {
+    return timestamp2WeeksAgo <= order.timestamp ? order : null; // filter all the 2 weeks ago
+  });
+  return ordersByLast2Weeks;
 };
 
-const getLast2WeeksOrders = () => {
-    //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+const bucketOrdersByDate = async () => {
+  let ordersByDate = {};
+  let orders = [];
+  //get all the last 2 weeks order
+  await getLast2WeeksOrders().then((data) => (orders = data));
+  //create list if hashmaps ,key is the user timeStemp ,value order
+  orders.map((order) =>
+    !ordersByUsers[order.timestamp]
+      ? (ordersByUsers[order.timestamp] = [order])
+      : ordersByUsers[order.timestamp].push(order)
+  );
+  debugger;
+  return ordersByDate;
 };
 
-const bucketOrdersByDate = () => {
-    let ordersByDate = {};
-    //   4. TODO: using the function from section 3 bucket the orders by date.
-    // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
-    return ordersByDate;
-};
+fetchAllOrders()
+  .then((data) => console.table(data))
+  .catch((err) => console.log(err));
 
-fetchAllOrders();
-// .then(console.log);
+bucketOrdersByUsers()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
 
-bucketOrdersByUsers();
-// .then(console.log);
+getLast2WeeksOrders()
+  .then((data) => console.table(data))
+  .catch((err) => console.log(err));
 
-getLast2WeeksOrders();
-// .then(console.log);
-
-bucketOrdersByDate();
-// .then(console.log);
+bucketOrdersByDate()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
 
 ////////////////////////////////////////
